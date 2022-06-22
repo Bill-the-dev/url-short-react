a60bc558d1424cea8419260e9280cb4022213cf5
bill-the-dev <billthedev@gmail.com>
2022-06-22 11:53:52 -0400

    Init repo, add API calls for group, group links, update group, and create shortlink

---
+++ bitly.md
@@ -0,0 +1,16 @@
+
+
+Access Token: 
+f303c835456ea4a6e7cc1bfaef70a05f57bdc453
+
+Group:
+o_71ptrgkcke (test-a-bill)  not true! use get request to find out pure group id
+[source](https://www.postman.com/speeding-spaceship-81846/workspace/bitly-api-testing/documentation/16940008-0cb6f8d4-f0b4-4e50-9d21-39a5dfacd87f)
+[axios example w/ auth](https://flaviocopes.com/axios-send-authorization-header/)
+
+Nested Lists on Click:
+https://germancruz.medium.com/display-nested-lists-in-reactjs-44a69c564fd9
+https://stackoverflow.com/questions/47289239/nested-list-using-react 
+
+Secret Keys in React:
+https://blog.devgenius.io/quick-tutorial-how-to-deal-with-secret-keys-in-react-apps-97e886c5d2a3
---
+++ src/App.jsx
@@ -0,0 +1,219 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const urlLink = 'https://api-ssl.bitly.com/v4/bitlinks';
+  const urlGroup = 'https://api-ssl.bitly.com/v4/groups';
+  const token = process.env.REACT_APP_BITLY_TOKEN
+  
+  const [currentGroup, setCurrentGroup] = useState({})
+  const [currentLinks, setCurrentLinks] = useState([])
+  const [groupsList, setGroupsList] = useState([])
+  const [longUrl, setLongUrl] = useState('')
+  
+  useEffect(() => {
+    // console.log(getShortLink())
+    getGroupsList()
+  }, [])
+
+  useEffect(() => {
+    if (groupsList.length > 0) {
+      getGroup(groupsList[0].guid)
+    }
+  }, [groupsList])
+
+  useEffect(() => {
+    if (currentGroup.guid) {
+      getGroupLinks(currentGroup.guid)
+    }
+  }, [currentGroup])
+
+  const getGroupsList = async() => {
+    try {
+      let response = await axios.get(urlGroup, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      })
+      // console.log(response.data.groups)
+      setGroupsList(response.data.groups)
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  const getGroup = async(groupId) => {
+    try {
+      let response = await axios.get(`${urlGroup}/${groupId}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      })
+      console.log(response.data)
+      setCurrentGroup(response.data)
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  // why link separate? Large data pull?
+  const getGroupLinks = async(groupId) => {
+    try {
+      let response = await axios.get(`${urlGroup}/${groupId}/bitlinks`, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log(response.data.links);
+      setCurrentLinks(response.data.links);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  const updateGroupName = async(groupId, form) => {
+    debugger
+    try {
+      let response = await axios.patch(`${urlGroup}/${groupId}`, form, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log(response.data);
+      setCurrentGroup(response.data);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  // fetch('https://api-ssl.bitly.com/v4/user', {
+  //   method: 'PATCH',
+  //   headers: {
+  //     'Authorization': 'Bearer {TOKEN}',
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify({ "name": "Chauncey McPufferson", "default_group_guid": "Ba1bc23dE4F" })
+  // });
+
+  const getShortLink = async() => {
+    try {
+      let response = await axios.post(urlLink, {
+        "long_url": "https://www.linkedin.com/in/bill-camarco/",
+        "domain": "bit.ly",
+        "group_guid": "Bm6lbqGKXai",
+        "title": "LinkedIN Profile",
+        "tags": [
+          "bitly",
+          "api"
+        ]
+      }, 
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      })
+      console.log(response.data)
+    } catch(error) {
+      alert(error)
+    }
+  };
+
+  // --- FORM METHODS ---
+
+  const [formValue, setFormValue] = useState({
+    name: currentGroup.name,
+  })
+
+  const handleSubmit = (type) => {
+    debugger
+    switch (type) {
+      case 'updateGroup':
+        debugger
+        return (e) => {
+          e.preventDefault();
+          debugger
+          updateGroupName(currentGroup.guid, formValue)
+        }
+    
+      default:
+        
+    }
+  }
+
+  const handleChange = (type) => {
+    return (e) => setFormValue({
+      ...formValue,
+      [type]: e.currentTarget.value
+    })
+
+  }
+
+  return (
+    
+    <main className="App"> 
+      {/* Group List */}
+      <h4>{`Groups (${groupsList.length}):`}</h4>
+      <ol> 
+        {(groupsList.length === 0)
+          ? <li>No Groups</li>
+          : groupsList.map((group, idx) => (
+            <li key={idx}>
+              <span>Name: {group.name}</span>
+              <br />
+              <span>Active: {(group.is_active) ? 'true' : 'false'}</span>  
+            </li>
+          ))
+        }
+      </ol>
+      <br />
+      
+      {/* Current Group Detail */}
+      <h4>Current Group: </h4>
+      <ul className='ul c-group-ul'>
+        <li>Name: {currentGroup.name}</li>
+        <li>ID: {currentGroup.guid}</li>
+        <li>Links: 
+          <ul className='ul c-group-links-ul'>
+            {(currentLinks.length === 0)
+              ? <li>No Links</li>
+              : currentLinks.map((link, idx) => (
+                <li key={idx}>
+                  <span>Title: {link.title}</span>
+                  <br />
+                  <span>Short Link: {<a href={link.link} target="_blank">{link.id}</a>}</span>
+                  <br />
+                  <span>Original Link: {<a href={link.long_url} target="_blank">{link.long_url}</a> }</span>
+                  <br />
+                  <span>Created At: {new Date(link.created_at).toLocaleString()}</span>
+                </li>
+              ))
+            }
+          </ul>
+        </li>
+      </ul>
+
+      {/* Current Link Metrics */}
+      <h4>Current Link Metrics</h4>
+
+      {/* Update Current Group */}
+      <h4>Update Group Details:</h4>  
+      <form onSubmit={handleSubmit('updateGroup')}>
+        <label>Group Name: 
+          <input type="text" defaultValue='' placeholder='Enter new name' id="group-name-input" onChange={handleChange("name")}/>
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+
+      </form>
+
+    </main>
+  );
+};
+
+export default App;
